2f27ce2c14f25a96331e12abb30e6c9cff10967f
Eric Larson <eric@ionrock.org>
2013-09-11 20:52:22 -0500

    Add bumpversion config and updated setup.py for README.rst

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
+readme = open('README.rst').read()
+
 
 setup_params = dict(
     name='MgoQuery',
-    version='0.3',
+    version='0.3.0',
     url='https://bitbucket.org/elarson/mgoquery',
     author='Eric Larson',
     author_email='eric@ionrock.org',
     py_modules=['mgoquery'],
     description='A concise language parser for creating MongoDB queries',
+    long_description=readme,
     install_requires=[
         'pyparsing==1.5.7'
     ],
