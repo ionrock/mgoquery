49679ed0b9dd4817376480c8f359a7e0be62a1a3
Eric Larson <eric@ionrock.org>
2013-09-11 20:54:32 -0500

    Update version to properly update based on pypi release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open('README.rst').read()
 
 setup_params = dict(
     name='MgoQuery',
-    version='0.3.0',
+    version='0.5.2',
     url='https://bitbucket.org/elarson/mgoquery',
     author='Eric Larson',
     author_email='eric@ionrock.org',
